afeb554f8364c3884b570aa219833c706cba59f8
SUKHSAGAR86 <sagarjangde86@gmail.com>
2025-10-14 17:21:04 +0530

    quick change navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -68,8 +68,7 @@ const NavBar = () => {
 
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
+              
               <Bars3Icon
                 aria-hidden="true"
                 className="block size-6 group-data-[open]:hidden"
